2ba8c870bd104ada885a00e1fec9a03890fce878
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-03 14:45:05 -0400

    separated into functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,73 @@
+import "./App.css";
+function Header() {
+  return (
+    <header>
+      <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+    </header>
+  );
+}
+
+/* New Recipe form moved into functioin*/
+function NewPost() {
+  return (
+    <div id="posts-new">
+      <h1>New post</h1>
+      <form>
+        <div>
+          Title: <input type="text" />
+        </div>
+        <div>
+          Body: <input type="text" />
+        </div>
+        <div>
+          Image: <input type="url" />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+function AllPosts() {
+  return (
+    <div id="posts-index">
+      <h1>All posts</h1>
+      <div className="recipes">
+        <h2>Coca-Cola</h2>
+        <p>Buy Coca-Cola and drink from bottle.</p>
+        <img src="#" alt="" />
+      </div>
+      <div className="recipes">
+        <h2>Snickers Bar</h2>
+        <p>Buy Snickers Bar, open and enjoy.</p>
+        <img src="#" alt="" />
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright 20XX</p>
+    </footer>
+  );
+}
+
+function Content() {
+  return (
+    <div>
+      <NewPost />
+      <AllPosts />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
-      </header>
-
-      {/* New Recipe form */}
-      <div id="posts-new">
-        <h1>New post</h1>
-        <form>
-          <div>
-            Title: <input type="text" />
-          </div>
-          <div>
-            Body: <input type="text" />
-          </div>
-          <div>
-            Image: <input type="url" />
-          </div>
-        </form>
-      </div>
-
-      <div id="posts-index">
-        <h1>All posts</h1>
-      </div>
-
-      <footer>
-        <p>Copyright 20XX</p>
-      </footer>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
