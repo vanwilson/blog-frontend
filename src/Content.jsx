39bbe067fb99fa25683815ad9034367b996e42dc
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-07 13:56:54 -0600

    connect backedn api to front end app

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import { AllPosts } from "./AllPosts";
 import { NewPost } from "./NewPost";
@@ -5,6 +6,15 @@ import { Modal } from "./Modal";
 
 export function Content() {
   const [isPostsShowVisible, setIsPostsShowVisible] = useState(false);
+
+  const [posts, setPosts] = useState([]);
+
+  const handleIndexPosts = () => {
+    axios.get("http://localhost:3000/posts.json").then((response) => {
+      console.log(response.data);
+      setPosts(response.data);
+    });
+  };
 
   const handleShowPost = () => {
     setIsPostsShowVisible(true);
@@ -14,32 +24,10 @@ export function Content() {
     setIsPostsShowVisible(false);
   };
 
-  let posts = [
-    {
-      id: 1,
-      title: "Same Old, Same Ol'",
-      body: "Yippy skippy!",
-      image:
-        "https://images.unsplash.com/photo-1589802829985-817e51171b92?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 2,
-      title: "Some More of the Same",
-      body: "Did I say this already?",
-      image:
-        "https://thumbs.dreamstime.com/b/aerial-view-lago-antorno-dolomites-lake-mountain-landscape-alps-peak-misurina-cortina-di-ampezzo-italy-reflected-103752677.jpg",
-    },
-    {
-      id: 3,
-      title: "I don't know either",
-      body: "This is what my 2 year olds says.",
-      image:
-        "https://hips.hearstapps.com/hmg-prod/images/alpe-di-siusi-sunrise-with-sassolungo-or-langkofel-royalty-free-image-1623254127.jpg",
-    },
-  ];
   return (
     <div>
       <NewPost />
+      <button onClick={handleIndexPosts}>Load Posts</button>
       <AllPosts posts={posts} onShowPost={handleShowPost} />
       <Modal show={isPostsShowVisible} onClose={handleClose}>
         <p>Welcome to my little slice of heaven.</p>
