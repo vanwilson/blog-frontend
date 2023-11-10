452b16b50764b605182fcccd7dde82a50245212a
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-10 13:16:04 -0600

    adds update button and form to postsShow

--- src/PostsShow.jsx
+++ src/PostsShow.jsx
@@ -1,10 +1,30 @@
+import axios from "axios";
+
 export function PostsShow(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    axios.patch(`http://localhost:3000/posts/${props.post.id}`, params).then((response) => {
+      console.log(response);
+      event.target.reset();
+    });
+  };
+
   console.log(props);
   return (
     <div id="posts-show">
-      <h2>{props.post.title}</h2>
-      <p>{props.post.body}</p>
-      <img src={props.post.image} alt="blog post image" />
+      <form onSubmit={handleSubmit}>
+        <div>
+          Title: <input defaultValue={props.post.title} name="title" type="text" />
+        </div>
+        <div>
+          Body: <input defaultValue={props.post.body} name="body" type="text" />
+        </div>
+        <div>
+          Image URL: <input defaultValue={props.post.image} name="image" type="text" />
+        </div>
+        <button>Update Post</button>
+      </form>
     </div>
   );
 }
