0c0fc79a757b9c39fa3a2ad67f02ff1b2f1ba5bd
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-12-04 14:27:46 -0600

    adds search bar to all posts

--- src/AllPosts.jsx
+++ src/AllPosts.jsx
@@ -1,20 +1,38 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
+
 export function AllPosts(props) {
   console.log(props);
+
+  const [searchFilter, setSearchFilter] = useState("");
   return (
     <div id="posts-index">
       <h1>All posts</h1>
+      <form className="d-flex" role="search">
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search"
+          value={searchFilter}
+          onChange={(event) => setSearchFilter(event.target.value)}
+        />
+        <button className="btn btn-outline-info" type="submit">
+          Search
+        </button>
+      </form>
       <div className="container text-center">
         <div className="row row-cols-3">
-          {props.posts.map((post) => (
-            <div key={post.id} className="col">
-              <div className="card">
-                <h2>{post.title}</h2>
-                {/* <p>{post.body}</p> */}
-                <img src={post.image} onClick={() => props.onShowPost(post)} />
+          {props.posts
+            .filter((post) => post.title.includes(searchFilter))
+            .map((post) => (
+              <div key={post.id} className="col">
+                <div className="card">
+                  <h2>{post.title}</h2>
+                  {/* <p>{post.body}</p> */}
+                  <img src={post.image} onClick={() => props.onShowPost(post)} />
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </div>
